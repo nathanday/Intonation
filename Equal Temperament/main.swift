09cbf2709ea6fb6f65660995f7b7de727862afbd
Nathan Day <nathan@Laptop.local>
2014-08-09 22:49:55 +1200

    Initial Commit

---
+++ Equal Temperament/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Equal Temperament
+//
+//  Created by Nathan Day on 9/08/14.
+//  Copyright (c) 2014 Nathan Day. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
---
+++ Equal TemperamentTests/Equal_TemperamentTests.swift
@@ -0,0 +1,36 @@
+//
+//  Equal_TemperamentTests.swift
+//  Equal TemperamentTests
+//
+//  Created by Nathan Day on 9/08/14.
+//  Copyright (c) 2014 Nathan Day. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Equal_TemperamentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
